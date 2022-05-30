175e691e829792e2ef1569578242d9437bef0621
David Milum <david.milum.89@gmail.com>
2022-05-30 18:02:17 -0400

    add format support

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/washtubs/upcoming"
 )
+
+var formatStringReference string = `
+type Upcoming struct {
+	Source       string
+	SourceId     string
+	Title        string
+	InvokeManual string
+	When         time.Time
+}
+`
 
 func main() {
 	flag.Parse()
@@ -18,10 +30,14 @@ func main() {
 	}
 	if action == "ls" {
 		fs := flag.NewFlagSet("ls", flag.ExitOnError)
-		var addresses string
-		var sources string
+		var (
+			addresses string
+			sources   string
+			format    string
+		)
 		fs.StringVar(&addresses, "addresses", "", "Comma separated list of server addresses")
 		fs.StringVar(&sources, "sources", "", "Comma separated list of sources")
+		fs.StringVar(&format, "format", "", fmt.Sprintf("Format string. Reference: %s", formatStringReference))
 		err := fs.Parse(flag.Args()[1:])
 		if err != nil {
 			log.Fatal(err)
@@ -39,8 +55,25 @@ func main() {
 			}
 		}
 		upcoming.SortByDuration(all)
+
+		var tmp *template.Template
+		if format != "" {
+			tmp = template.New("format")
+			tmp, err = tmp.Parse(format)
+			if err != nil {
+				fs.Usage()
+				log.Fatal(err)
+			}
+		}
+
+		out := os.Stdout
 		for _, v := range all {
-			fmt.Println(upcoming.Format(v))
+			if format == "" {
+				fmt.Fprintln(out, upcoming.Format(v))
+			} else {
+				tmp.Execute(out, v)
+				fmt.Fprintln(out)
+			}
 		}
 	} else if action == "rm" {
 		var (
@@ -52,6 +85,14 @@ func main() {
 		fs.StringVar(&source, "source", "", "Remove all for a given source")
 		fs.StringVar(&sourceId, "id", "", "Remove the given source ID")
 		fs.StringVar(&address, "address", "", "Server address")
+		err := fs.Parse(flag.Args()[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if source == "" {
+			fs.Usage()
+			log.Fatal("Please provide a -source")
+		}
 
 		client := upcoming.NewClient(address)
 		if sourceId != "" {
